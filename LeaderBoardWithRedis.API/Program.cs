a36224431c1a8bcc59329c0e0dda00b884d7bad0
Armin Sharifi <Arminsharifi4@gmail.com>
2023-07-22 18:10:12 +0330

    Change the logger.Debug to logger.Info

--- LeaderBoardWithRedis.API/Program.cs
+++ LeaderBoardWithRedis.API/Program.cs
@@ -5,7 +5,7 @@ using NLog;
 using NLog.Web;
 
 var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
-logger.Debug("init main");
+logger.Info("init main");
 try
 {
     var builder = WebApplication.CreateBuilder(args);
